06d2620540b6f89dad35c97db872c6e1dd72a6e7
kinio-1998 <li.carlosduarte98@gmail.com>
2025-05-05 14:56:50 -0700

    se agrego un nuevo certificado

--- src/obj/objCertifications.js
+++ src/obj/objCertifications.js
@@ -9,17 +9,24 @@ export const certifications ={
         },
         {
             id:2,
-            institution:"EF SET English Certificate",
-            title:"(59/100 - B2 Upper Intermediate)",
+            institution:"IBM",
+            title:"Python for Data Science",
             date:"Jun 2024",
             link:""
         },
         {
             id:3,
-            institution:"EF SET English Certificate",
-            title:"(59/100 - B2 Upper Intermediate)",
-            date:"Jun 2024",
+            institution:"Universidad Autónoma de Sinaloa",
+            title:"Diplomado en Desarrolo web con Infraestructura de servicios y multiservicios en la nube",
+            date:"Ago 2021",
             link:""
+        },
+        {
+            id:4,
+            institution:"Desafio Latam",
+            title:"Javascript Interactivo",
+            date:"Nov 2024",
+            link:"https://tutorial-sql.s3.amazonaws.com/certificates/28608_Carlos%20daniel_Duarte%20le%C3%B3n/28608_02092024.png"
         },
     ],
     en:[        
